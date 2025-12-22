b2cea482b0685596d86b469aad320f1ca59c2d2c
Rishika Gautam <rishikagautam13@gmail.com>
2025-12-22 14:52:13 -0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,9 @@
+window.env = {
+    API_URL: 'http://127.0.0.1:8000'
+    /*
+    API_URL: 'http://54.215.135.110', //Use Public IPv4 Address
+    EMAILJS_PUBLIC_KEY: 'gFmPbcjw7xdJQHdak',
+    EMAILJS_SERVICE_ID: 'service_dietnerd',
+    EMAILJS_TEMPLATE_ID: 'contact_form'
+    */
+}
